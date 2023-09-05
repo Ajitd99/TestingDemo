46b00c4d457faa6da2d40e3c8926c0a1c4420c8b
Ajitd99 <ajit.d@amazatic.com>
2023-09-05 22:31:26 +0530

    commit 23

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ cd      C:\Users\User\PycharmProjects\TestAutomation
 ::pabot --processes 2 --outputdir Result TestCases\POMLogin.robot  TestCases\ART.robot
 ::pabot --processes 1 TestCases\Registration.robot
 ::robot CMS1\
-  pabot --processes 9 CMS1\*.robot
+  pabot --processes 2 CMS1\*.robot
